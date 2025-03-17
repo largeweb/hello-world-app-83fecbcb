444dc094f565e2f3da5e5a2ed2c90a2e54614dbe
largeweb <77933287+largeweb@users.noreply.github.com>
2025-03-17 16:04:02 -0400

    Add app/components/HelloWorld.tsx

---
+++ app/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+// app/components/HelloWorld.tsx
+
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div className="text-3xl font-bold text-gray-800">
+      Hello, World!
+    </div>
+  );
+};
+
+export default HelloWorld;
